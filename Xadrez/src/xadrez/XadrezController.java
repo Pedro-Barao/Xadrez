5bcf4debd2d6cab44918700087ae9ff583b59faf
tk <53953743+TkeX1@users.noreply.github.com>
2025-10-07 09:57:03 -0300

    Add files via upload

---
+++ Xadrez/src/xadrez/XadrezController.java
@@ -0,0 +1,18 @@
+package calculadora;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author user
+ */
+public class XadrezController {
+
+    @FXML
+    private String String;
+    @FXML
+    private TextField Texto;
+}
